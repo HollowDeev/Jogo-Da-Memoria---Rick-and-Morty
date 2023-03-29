453865fdc8f25cf5d087a8df6e47d3681a758c09
Thaua <thaua19felipe@gmail.com>
2023-03-29 01:50:27 -0300

    Projeto Finalizado

---
+++ js/game.js
@@ -0,0 +1,131 @@
+const grid = document.querySelector('.grid')
+const player = document.querySelector('.player')
+const timer = document.querySelector('.timer')
+
+const characters = [
+    'beth',
+    'jerry',
+    'jessica',
+    'morty',
+    'pessoa-passaro',
+    'pickle-rick',
+    'rick',
+    'summer',
+    'meeseeks',
+    'scroopy',
+]
+
+const createElement = (tag, className) => {
+    const element = document.createElement(tag)
+    element.className = className
+    return element
+}
+
+let firstCard = '';
+let secondCard = '';
+
+const verifyEndGame = () => {
+    const disabledCards = document.querySelectorAll('.disabled-card')
+
+    if (disabledCards.length == 20) {
+        setTimeout(() => {
+            alert('Parabens ' + player.innerHTML + ', voce conseguiu em: ' + timer.innerHTML + ' segundos')
+            clearInterval(this.loop)
+        }, 300)
+        
+       
+    }
+}
+
+const verifyCards = () => {
+    const firstCharacter = firstCard.getAttribute('data-character')
+    const secondCharacter = secondCard.getAttribute('data-character')
+
+    if (firstCharacter == secondCharacter) {
+
+        firstCard.firstChild.classList.add('disabled-card')
+        secondCard.firstChild.classList.add('disabled-card')
+
+        firstCard = ''
+        secondCard = ''
+
+        verifyEndGame()
+
+    } else {
+        
+        setTimeout(() => {
+            firstCard.classList.remove('reveal-card')
+            secondCard.classList.remove('reveal-card')
+
+            firstCard = ''
+            secondCard = ''
+        }, 500)
+        
+    }
+}
+
+
+const revealCard = ({target}) => {
+    
+    if (target.parentNode.className.includes('reveal-card')) {
+        return
+    }
+
+    if (firstCard === '') {
+        target.parentNode.classList.add('reveal-card')
+        firstCard = target.parentNode
+    } else if (secondCard === '') {
+        target.parentNode.classList.add('reveal-card')
+        secondCard = target.parentNode
+
+        verifyCards()
+
+    }
+
+}
+
+
+const createCard = (character) => {
+
+    const card = createElement('div', 'card')
+    const front = createElement('div', 'face front')
+    const back = createElement('div', 'face back')
+
+    front.style.backgroundImage = "url('../images/" + character + ".png')"
+
+    card.appendChild(front)
+    card.appendChild(back)
+
+    card.addEventListener('click', revealCard)
+    card.setAttribute('data-character', character)
+    
+    return card
+}
+
+const loadGame = () => {
+
+    const duplicadeCharacters = [...characters, ...characters]
+
+    const shuffledCharacters = duplicadeCharacters.sort( () => Math.random() - 0.5)
+
+    shuffledCharacters.forEach((characters) => {
+        const card = createCard(characters)
+        grid.appendChild(card)
+    })
+
+}
+
+const startTimer = () => {
+    this.loop = setInterval(() => {
+
+        const currentTime = +timer.innerHTML
+        timer.innerHTML = currentTime + 1
+
+    }, 1000)
+}
+
+window.onload = () => {
+    player.innerHTML = localStorage.getItem('playerName')
+    startTimer()
+    loadGame()
+}
